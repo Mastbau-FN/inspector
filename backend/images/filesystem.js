2c63eedfb60a78eb1a7426d469148cdc099a97b0
hannes <33062605+HannesGitH@users.noreply.github.com>
2021-09-29 16:04:46 +0200

    ALL_CAPS on env-vars for some reason needed on their end

---
+++ backend/images/filesystem.js
@@ -0,0 +1,50 @@
+const fs = require('fs');
+const fsp = fs.promises;
+
+const path = require('path');
+
+
+const root_path = process.env.IMG_ROOT_PATH; //might be needed when mounting network drives locally
+
+const _getImageFromPath = async (path) => {
+    //TODO get image from network (siehe [#9](https://github.com/Mastbau-FN/inspector/issues/9))
+    return await fsp.readFile(path);
+}
+
+const getImageFrom = async (rootpath,link,filename)=>{
+    //TODO how to merge the specific paths and where do they come from?
+    return await _getImageFromPath(path.join(rootpath,link,filename));
+}
+
+
+const _getAllImagenamesFromPath = async (path) => {
+    //TODO get image from network (siehe [#9])
+    return await fsp.readdir(path);
+}
+
+const getAllImagenamesFrom = async (rootpath, link) => {
+    //TODO how to merge the specific paths and where do they come from?
+    return await _getAllImagenamesFromPath(path.join(rootpath,link))
+}
+
+module.exports = {
+    getAllImagenamesFrom,
+    getImageFrom
+}
+
+// MARK: deprecated
+
+// const _getAllFilesInPath = async (path) => {
+//     //TODO get image from network (siehe #9)
+//     const filenames = await fsp.readdir(path);
+
+//     const results = [];
+//     filenames.forEach((filename) => {
+//         results.push(_getImageFromPath(path+filename));
+//     });
+//     return await Promise.all(results);
+// }
+// const getAllImagesFrom = async (rootpath, link) => {
+//     //TODO how to merge the specific paths and where do they come from?
+//     return await _getAllFilesInPath(rootpath+link)
+// }
--- backend/index.js
+++ backend/index.js
@@ -21,7 +21,7 @@ const auth = require('./auth/auth')
 
 
 const app = express()
-const port = process.env.port || 443
+const port = process.env.PORT || 443
 
 app.use(express.json())
 app.use(
@@ -44,7 +44,7 @@ app.get('/', (request, response) => {
  * very important later though, since otherwise everybody can read and write data 
  * its also very important to only expose this api via https since otherwise the auth data wouldn't be encrypted and anyone could steal the credentials
 */
-if (!process.env.insecure){
+if (!isInsecure){
     // api-key wall
     app.use('/api/secure/', auth.api_wall);
     // needs a user to be logged in aka provided via the user param inside the post request 
@@ -107,7 +107,7 @@ if(isInsecure){
     console.warn(`App running on port ${port}. THIS IS INSECURE`)//TODO remove http and force https
   })
 }else{
-  const cert_path = process.env.cert_path;
+  const cert_path = process.env.CERT_PATH;
   const key = fs.readFileSync(cert_path+'privkey.pem');
   const cert = fs.readFileSync(cert_path+'cert.pem');
   var httpsServer = https.createServer({key: key, cert: cert}, app);
