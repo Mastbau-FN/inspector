831062f909b4c3b1110d52c7b1f35f8cd9ac9dd3
hannes <33062605+HannesGitH@users.noreply.github.com>
2021-10-05 17:06:08 +0200

    formatpath

--- backend/images/filesystem.js
+++ backend/images/filesystem.js
@@ -6,8 +6,11 @@ const pathm = require('path');
 
 const root_path = process.env.IMG_ROOT_PATH; //might be needed when mounting network drives locally
 
+///removes the drive and replaces it with our given root path
 const _formatpath = (path) => {
-    pathm.
+    const parts = path.split(pathm.sep);
+    parts.shift();
+    pathm.join(root_path,...parts);
 };
 
 
@@ -19,7 +22,7 @@ const _getImageFromPath = async (path) => {
 
 const getImageFrom = async (rootpath,link,filename)=>{
     //TODO how to merge the specific paths and where do they come from?
-    return await _getImageFromPath(pathm.join(rootpath,link,filename));
+    return await _getImageFromPath(pathm.join(_formatpath(rootpath),link,filename));
 }
 
 
@@ -34,7 +37,7 @@ const _getAllImagenamesFromPath = async (path) => {
 const getAllImagenamesFrom = async (rootpath, link) => {
     //TODO how to merge the specific paths and where do they come from?
     ////console.log(rootpath);console.log(link);
-    try {return await _getAllImagenamesFromPath(pathm.join(rootpath,link))}catch(e){return []}
+    try {return await _getAllImagenamesFromPath(pathm.join(_formatpath(rootpath),link))}catch(e){return []}
 }
 
 module.exports = {
