9837aed88b54fb19fbfe702a123722e0698b07b4
Hannes <33062605+HannesGitH@users.noreply.github.com>
2021-11-26 13:00:07 +0100

    started editing of editing

--- frontend/lib/pages/detailsPage.dart
+++ frontend/lib/pages/detailsPage.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 
-class DetailsPage extends StatelessWidget {
+class DetailsPage extends StatefulWidget {
   final String title;
   final String? details;
   final void Function(String) onChanged;
@@ -10,20 +10,36 @@ class DetailsPage extends StatelessWidget {
       : super(key: key);
 
   @override
+  State<DetailsPage> createState() => _DetailsPageState();
+}
+
+class _DetailsPageState extends State<DetailsPage> {
+  bool _isEditing = false;
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text(title),
+        title: Text(widget.title),
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: () {}, //TODO: edit text
-        child: Icon(Icons.edit),
+        onPressed: () {
+          setState(() {
+            if (_isEditing) widget.onChanged("TODO: neuer String");
+            _isEditing ^= true;
+          });
+        }, //TODO: edit text
+        child: Icon(_isEditing ? Icons.done : Icons.edit),
         tooltip: "noch in arbeit..",
       ),
       body: Padding(
         padding: EdgeInsets.all(10),
-        child: Text(details ?? ""),
+        child: _body,
       ),
     );
   }
+
+  Widget get _body => _isEditing ? _inEdit : _inShow;
+
+  Widget get _inEdit => TextField();
+  Widget get _inShow => Text(widget.details ?? "");
 }
