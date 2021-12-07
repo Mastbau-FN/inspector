b33837d1e27f40b14c08e1bc0d24a304cf821fef
Hannes <33062605+HannesGitH@users.noreply.github.com>
2021-12-07 16:13:49 +0100

    auch so eher net

--- frontend/lib/pages/detailsPage.dart
+++ frontend/lib/pages/detailsPage.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_quill/flutter_quill.dart' as q;
 
-class DetailsPage extends StatelessWidget {
+class DetailsPage extends StatefulWidget {
   final String title;
   final bool isRich;
   final String? details;
@@ -9,9 +9,13 @@ class DetailsPage extends StatelessWidget {
   final void Function(String)? onChanged;
   final void Function(List<dynamic>)? onRichChanged;
 
-  const DetailsPage(
-      {Key? key, required this.title, this.details, required this.onChanged})
-      : this.isRich = false,
+  const DetailsPage({
+    Key? key,
+    required this.title,
+    this.details,
+    required void Function(String) onChanged,
+  })  : this.isRich = false,
+        this.onChanged = onChanged,
         this.onRichChanged = null,
         this.richDetails = null,
         super(key: key);
@@ -19,44 +23,17 @@ class DetailsPage extends StatelessWidget {
   const DetailsPage.rich(
       {Key? key,
       required this.title,
-      this.richDetails,
+      required List<dynamic> details,
       required void Function(List<dynamic>) onChanged})
       : this.isRich = true,
         this.onRichChanged = onChanged,
+        this.richDetails = details,
         this.onChanged = null,
         this.details = null,
         super(key: key);
 
   @override
-  Widget build(BuildContext context) => isRich ? _RichEditor() : _BasicEditor();
-}
-
-class _BasicEditor extends StatefulWidget {
-  _BasicEditor({Key? key}) : super(key: key);
-
-  @override
-  __BasicEditorState createState() => __BasicEditorState();
-}
-
-class __BasicEditorState extends State<_BasicEditor> {
-  @override
-  Widget build(BuildContext context) {
-    return Container();
-  }
-}
-
-class _RichEditor extends StatefulWidget {
-  _RichEditor({Key? key}) : super(key: key);
-
-  @override
-  __RichEditorState createState() => __RichEditorState();
-}
-
-class __RichEditorState extends State<_RichEditor> {
-  @override
-  Widget build(BuildContext context) {
-    return Container();
-  }
+  State<DetailsPage> createState() => _DetailsPageState();
 }
 
 class _DetailsPageState extends State<DetailsPage> {
@@ -73,7 +50,7 @@ class _DetailsPageState extends State<DetailsPage> {
             debugPrint(_controller.document.toDelta().toJson().toString());
             if (_isEditing) {
               widget.isRich
-                  ? widget.onChanged!.call(_controller.document.toPlainText())
+                  ? widget.onChanged!.call(_basicController.text)
                   : widget.onRichChanged!
                       .call(_controller.document.toDelta().toJson());
             }
@@ -92,25 +69,39 @@ class _DetailsPageState extends State<DetailsPage> {
   }
 
   Widget get _body => _isEditing ? _inEdit : _inShow;
+  Widget get _inShow => widget.isRich ? _richShow : _plainShow;
+  Widget get _inEdit => widget.isRich ? _richEdit : _plainEdit;
 
-  q.QuillController get _controller => q.QuillController(
-        document: q.Document.fromJson(
-            widget.richDetails!), //widget.isRich ? widget.details : null,
-        selection: TextSelection.collapsed(offset: 0),
+  Widget get _plainShow {
+    return Text(widget.details ?? "");
+  }
+
+  Widget get _plainEdit => TextFormField(
+        initialValue: widget.details ?? "",
+        controller: _basicController,
       );
 
-  Widget get _inEdit => Column(
+  Widget get _richEdit => _rich(inEdit: true);
+  Widget get _richShow => _rich(inEdit: false);
+
+  Widget _rich({required bool inEdit}) => Column(
         children: [
           q.QuillToolbar.basic(locale: Locale('de'), controller: _controller),
           Expanded(
             child: Container(
               child: q.QuillEditor.basic(
                 controller: _controller,
-                readOnly: false, // true for view only mode
+                readOnly: !inEdit,
               ),
             ),
           )
         ],
       );
-  Widget get _inShow => Text(widget.details ?? "");
+
+  TextEditingController get _basicController => TextEditingController();
+  q.QuillController get _controller => q.QuillController(
+        document: q.Document.fromJson(
+            widget.richDetails!), //widget.isRich ? widget.details : null,
+        selection: TextSelection.collapsed(offset: 0),
+      );
 }
