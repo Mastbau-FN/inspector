45aa9d13eed0ba619504e37bf006def6a909da6d
Hannes-t510 <33062605+HannesGitH@users.noreply.github.com>
2021-12-12 21:08:40 +0100

    removing umlaute (still for #58)

--- backend/extern/location.js
+++ backend/extern/location.js
@@ -48,4 +48,27 @@ module.exports = {
     addLocations,
 };
 
-const _locationToString = (location)=>`${location.Straße}, ${location.Straße}, ${location.Straße}`;
+const _locationToString = (location)=>replaceUmlaute(`${location.Straße}, ${location.Straße}, ${location.Straße}`);
+
+
+//see https://stackoverflow.com/a/54346022
+const umlautMap = {
+    '\u00dc': 'UE',
+    '\u00c4': 'AE',
+    '\u00d6': 'OE',
+    '\u00fc': 'ue',
+    '\u00e4': 'ae',
+    '\u00f6': 'oe',
+    '\u00df': 'ss',
+  }
+
+  function replaceUmlaute(str) {
+    return str
+      .replace(/[\u00dc|\u00c4|\u00d6][a-z]/g, (a) => {
+        const big = umlautMap[a.slice(0, 1)];
+        return big.charAt(0) + big.charAt(1).toLowerCase() + a.slice(1);
+      })
+      .replace(new RegExp('['+Object.keys(umlautMap).join('|')+']',"g"),
+        (a) => umlautMap[a]
+      );
+  }
