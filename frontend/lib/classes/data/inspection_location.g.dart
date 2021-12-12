6ec3912efdc7fcecf468f56da6e7657a06805ab6
Hannes-t510 <33062605+HannesGitH@users.noreply.github.com>
2021-12-12 20:25:14 +0100

    same

--- frontend/lib/classes/data/inspection_location.g.dart
+++ frontend/lib/classes/data/inspection_location.g.dart
@@ -6,23 +6,21 @@ part of 'inspection_location.dart';
 // JsonSerializableGenerator
 // **************************************************************************
 
-InspectionLocation _$InspectionLocationFromJson(Map<String, dynamic> json) {
-  debugPrint(json.toString());
-  return InspectionLocation(
-    bauleitung: json['Bauleitung'] as String?,
-    ort: json['Ort'] as String?,
-    pjInfo: json['PjInfo'] as String?,
-    pjName: json['PjName'] as String?,
-    pjNr: json['PjNr'] as int,
-    plz: json['PLZ'] as String?,
-    stONr: json['StONr'] as int,
-    strasse: json['Straße'] as String?,
-    coords: json['latLng'] == null
-        ? null
-        : LatLng.fromJson(json['latLng'] as Map<String, dynamic>),
-  )..imagehashes =
-      (json['images'] as List<dynamic>?)?.map((e) => e as String).toList();
-}
+InspectionLocation _$InspectionLocationFromJson(Map<String, dynamic> json) =>
+    InspectionLocation(
+      bauleitung: json['Bauleitung'] as String?,
+      ort: json['Ort'] as String?,
+      pjInfo: json['PjInfo'] as String?,
+      pjName: json['PjName'] as String?,
+      pjNr: json['PjNr'] as int,
+      plz: json['PLZ'] as String?,
+      stONr: json['StONr'] as int,
+      strasse: json['Straße'] as String?,
+      coords: json['latLng'] == null
+          ? null
+          : LatLng.fromJson(json['latLng'] as Map<String, dynamic>),
+    )..imagehashes =
+        (json['images'] as List<dynamic>?)?.map((e) => e as String).toList();
 
 Map<String, dynamic> _$InspectionLocationToJson(InspectionLocation instance) =>
     <String, dynamic>{
