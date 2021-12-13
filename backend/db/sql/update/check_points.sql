94e0c633de0377a12343b2a39b1e423dac75aa49
MbgBn <46566535+MbgBn@users.noreply.github.com>
2021-12-13 15:14:53 +0100

    Update check_points.sql

--- backend/db/sql/update/check_points.sql
+++ backend/db/sql/update/check_points.sql
@@ -1,21 +1,14 @@
--- add Prüfpunkt (04_Auswahl_Prüfpunkte)
-INSERT INTO "Events" ("PjNr", "KurzText", "LangText", "Link", "LinkOrdner", "EventID", "EREArt", "E1", "E2", "E3", "Insp_Stelle") 
-VALUES (
-$1, /*Projektnummer*/
-$3, /*name (required)*/
-$4, /*description (optional)*/
-$5, /*Link*/
-$6, /*linkOrdner*/
-0, /*erstmal 0, später mehr*/
-5200, /*is PrüfPunkt*/
-$2, /*required: Wert der Ebene 1*/
-(SELECT COALESCE("E2",0) + 1 AS "E2"
-FROM "Events"
-WHERE "PjNr" = $1 /*required: Projektnummer*/
-AND "E1" = $2 /*required: Wert der Ebene 1*/
-ORDER BY COALESCE("E2",0) DESC LIMIT 1),
-0,
-NULL
-)
-RETURNING "E2"
+-- update Prüfpunkt (04_Auswahl_Prüfpunkte)
+UPDATE "Events"  
+SET 
+"PjNr" = $1,
+"KurzText" = §4,
+"LangText" =$5,
+"Link" = $8,
+"LinkOrdner" = $9,
+"Insp_Stelle" = $? /*ggf. neuer Parameter?*/
+WHERE
+"EREArt" = 5200 /*Prüfpunkt*/
+AND "E1" = $? /*neuer Parameter*/
+AND "E2" = $? /*neuer Parameter*/
 ;
