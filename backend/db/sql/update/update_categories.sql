312f069fdc3e3fd3ac991eb885450d9706bd832b
MbgBn <46566535+MbgBn@users.noreply.github.com>
2021-12-13 15:15:45 +0100

    Update and rename check_categories.sql to update_categories.sql

--- backend/db/sql/update/update_categories.sql
+++ backend/db/sql/update/update_categories.sql
@@ -2,11 +2,11 @@
 UPDATE "Events"  
 SET 
 "PjNr" = $1,
-"KurzText" = §2
-"LangText" =$3
-"Link" = $3,
-"LinkOrdner" = $4
-"Insp_Stelle" = $? /*ggf. neuer Parameter*/
+"KurzText" = §4,
+"LangText" =$5,
+"Link" = $8,
+"LinkOrdner" = $9,
+"Insp_Stelle" = $? /*ggf. neuer Parameter?*/
 WHERE
 "EREArt" = 5100 /*Prüfkategorie*/
 AND "E1" = $? /*neuer Parameter*/
