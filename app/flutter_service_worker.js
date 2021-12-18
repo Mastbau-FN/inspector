a6ec2a4ec6855404785d8e4e12eccd6fe03fca92
HannesGitH <33062605+HannesGitH@users.noreply.github.com>
2021-12-18 10:56:58 +0000

    Deploying to gh-pages from @ Mastbau-FN/inspector@d4437010c0672a20219c0dcfba89dcdaedd5cc36 🚀

--- app/flutter_service_worker.js
+++ app/flutter_service_worker.js
@@ -27,8 +27,8 @@ const RESOURCES = {
 "assets/packages/fluttertoast/assets/toastify.js": "e7006a0a033d834ef9414d48db3be6fc",
 "assets/packages/youtube_player_flutter/assets/speedometer.webp": "50448630e948b5b3998ae5a5d112622b",
 "assets/packages/cupertino_icons/assets/CupertinoIcons.ttf": "6d342eb68f170c97609e9da345464e5e",
-"index.html": "261113ebe295709c5ebb0214aec5c84d",
-"/": "261113ebe295709c5ebb0214aec5c84d",
+"index.html": "78bf8965378eb0320bff46056cd51421",
+"/": "78bf8965378eb0320bff46056cd51421",
 "favicon.png": "b0d744ab17e2391c03cb527550c7c3cd"
 };
 
