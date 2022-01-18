32cce1bee9ed02c1fe1db2af9787e01465abb6ae
Hannes vonSarahsLaptop <HannesGitH@users.noreply.github.com>
2022-01-18 16:07:55 +0100

    conformed api to prev commit

--- backend/api.js
+++ backend/api.js
@@ -98,7 +98,7 @@ const deleteImgByHash = (req, res, next) =>
   );
 
   const setMainImgByHash = (req, res, next) => {
-    const pathparts = imghasher.getPathFromHash(hash);
+    const pathparts = imghasher.getPathFromHash(req.body.hash);
     req.body.data.Link = path.join(pathparts.Link,pathparts.filename); // LinkOrdner+/+filename
     return update(req,res,next);
   }
