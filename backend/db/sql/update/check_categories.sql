969b7163e90ba137418d394a39c0ca1d6896d116
MbgBn <46566535+MbgBn@users.noreply.github.com>
2022-02-21 12:39:53 +0100

    Update check_categories.sql

--- backend/db/sql/update/check_categories.sql
+++ backend/db/sql/update/check_categories.sql
@@ -1,15 +1,30 @@
 -- update Categorie (03_Auswahl_Prüfkategorie)
-UPDATE "Events"  
-SET 
-"PjNr" = $1,
-"KurzText" = $3,
-"LangText" =$4,
-"Link" = $5,
-"LinkOrdner" = $6,
-"Zusatz_Info" = $7 /*ggf. neuer Parameter?*/
-WHERE
-"EREArt" = 5100 /*Prüfkategorie*/
-AND ("E1" = $2 /*neuer Parameter*/
-AND ("E2" = 0 OR "E2" is null)
-AND ("E3" = 0 OR "E3" is null))
-;
+with events_update as (
+	UPDATE "Events"  
+		SET 
+		"PjNr" = $1,
+		"KurzText" = $3,
+		"LangText" =$4,
+		"Link" = $5,
+		"LinkOrdner" = $6,
+		"Zusatz_Info" = $7 /*ggf. neuer Parameter?*/
+		WHERE
+		"EREArt" = 5100 /*Prüfkategorie*/
+		AND ("E1" = $2 /*neuer Parameter*/
+		AND ("E2" = 0 OR "E2" is null)
+		AND ("E3" = 0 OR "E3" is null))
+	returning "PjNr"
+)
+UPDATE "Insp_STANDORT_Infos" 
+	"Eigentuemer"=$?, 
+	"Bauwerkhoehe"=$?, 
+	"Baujahr"=$?, 
+	"Ansprechpartner"=$?, 
+	"Steigwegtyp"=$?, 
+	"Schluessel"=$?, 
+	"Abschaltungen"=$?, 
+	"Steckdosen"=$?, 
+	"WC"=$?, 
+	"Lagerraeume"=$?, 
+	"Steigschutzschluessel"=$?
+WHERE ("PjNr") IN (select "PjNr" from events_update);
