3486dad76c01bb2087d848345a156a71f4696045
MbgBn <46566535+MbgBn@users.noreply.github.com>
2022-02-21 16:20:18 +0100

    Update check_categories.sql

--- backend/db/sql/update/check_categories.sql
+++ backend/db/sql/update/check_categories.sql
@@ -1,30 +1,15 @@
 -- update Categorie (03_Auswahl_Prüfkategorie)
-with events_update as (
-	UPDATE "Events"  
-		SET 
-		"PjNr" = $1,
-		"KurzText" = $3,
-		"LangText" =$4,
-		"Link" = $5,
-		"LinkOrdner" = $6,
-		"Zusatz_Info" = $7 /*ggf. neuer Parameter?*/
-		WHERE
-		"EREArt" = 5100 /*Prüfkategorie*/
-		AND ("E1" = $2 /*neuer Parameter*/
-		AND ("E2" = 0 OR "E2" is null)
-		AND ("E3" = 0 OR "E3" is null))
-	returning "PjNr"
-)
-UPDATE "Insp_STANDORT_Infos" 
-	"Eigentuemer"=$?, 
-	"Bauwerkhoehe"=$?, 
-	"Baujahr"=$?, 
-	"Ansprechpartner"=$?, 
-	"Steigwegtyp"=$?, 
-	"Schluessel"=$?, 
-	"Abschaltungen"=$?, 
-	"Steckdosen"=$?, 
-	"WC"=$?, 
-	"Lagerraeume"=$?, 
-	"Steigschutzschluessel"=$?
-WHERE ("PjNr") IN (select "PjNr" from events_update);
+UPDATE "Events"  
+SET 
+"PjNr" = $1,
+"KurzText" = $3,
+"LangText" =$4,
+"Link" = $5,
+"LinkOrdner" = $6,
+"Zusatz_Info" = $7 /*ggf. neuer Parameter?*/
+WHERE
+"EREArt" = 5100 /*Prüfkategorie*/
+AND ("E1" = $2 /*neuer Parameter*/
+AND ("E2" = 0 OR "E2" is null)
+AND ("E3" = 0 OR "E3" is null))
+;
