5f1848b411f56c4003e3d654196bd460eff45b15
HannesH <33062605+HannesGitH@users.noreply.github.com>
2022-04-25 18:43:10 +0200

    i said preview

--- frontend/lib/fragments/weather/editableWeatherView.dart
+++ frontend/lib/fragments/weather/editableWeatherView.dart
@@ -23,12 +23,75 @@ class WeatherPreview extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Row(
+      children: [
+        WeatherIcon(weather: weatherData.weather),
+        WindPreview(weatherData: weatherData),
+        TemperaturePreview(temperature: weatherData.temperature),
+      ],
+    );
+  }
+}
+
+class TemperaturePreview extends StatelessWidget {
+  final int? temperature;
+  const TemperaturePreview({required this.temperature, Key? key})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return temperature != null
+        ? Text(
+            "$temperature°C",
+            style: TextStyle(fontSize: 20),
+          )
+        : Container();
+  }
+}
+
+class WindPreview extends StatelessWidget {
+  final WeatherData weatherData;
+  const WindPreview({required this.weatherData, Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        WindDirectionIcon(dir: weatherData.wind_direction),
+        WindSpeedIcon(windSpeed: weatherData.wind_speed)
+      ],
+    );
+  }
+}
+
+class WindSpeedIcon extends StatelessWidget {
+  final WindPower? windSpeed;
+  const WindSpeedIcon({Key? key, required this.windSpeed}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Text(windSpeed == WindPower.none
+        ? "windstill"
+        : windSpeed == WindPower.medium
+            ? "mäßiger wind"
+            : windSpeed == WindPower.strong
+                ? "starker wind"
+                : "unbekannter wind");
+  }
+}
+
+class WindDirectionIcon extends StatelessWidget {
+  final WindDirection? dir;
+  const WindDirectionIcon({Key? key, required this.dir}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Icon(windDir2icon(dir));
   }
 }
 
 class WeatherIcon extends StatelessWidget {
-  final Weather weather;
+  final Weather? weather;
   const WeatherIcon({required this.weather, Key? key}) : super(key: key);
 
   @override
--- frontend/lib/fragments/weather/helpers.dart
+++ frontend/lib/fragments/weather/helpers.dart
@@ -1,8 +1,9 @@
 import 'package:MBG_Inspektionen/classes/data/weather.dart';
+import 'package:MBG_Inspektionen/fragments/weather/editableWeatherView.dart';
 import 'package:flutter/material.dart';
 import 'package:weather_icons/weather_icons.dart';
 
-IconData weather2icon(Weather w) => w == Weather.slightly_rainy
+IconData weather2icon(Weather? w) => w == Weather.slightly_rainy
     ? WeatherIcons.rain_mix
     : w == Weather.rain
         ? WeatherIcons.rain
@@ -22,4 +23,45 @@ IconData weather2icon(Weather w) => w == Weather.slightly_rainy
                                     ? WeatherIcons.fog
                                     : w == Weather.storm
                                         ? WeatherIcons.strong_wind
-                                        : Icons.question_mark;
+                                        : WeatherIcons.na;
+
+String windDir2string(WindDirection? wd) {
+  switch (wd) {
+    case WindDirection.north:
+      return "N";
+    case WindDirection.north_north_east:
+      return "NNO";
+    case WindDirection.north_east:
+      return "NO";
+    case WindDirection.east_north_east:
+      return "ONO";
+    case WindDirection.east:
+      return "O";
+    case WindDirection.east_south_east:
+      return "OSO";
+    case WindDirection.south_east:
+      return "SO";
+    case WindDirection.south_south_east:
+      return "SSO";
+    case WindDirection.south:
+      return "S";
+    case WindDirection.south_south_west:
+      return "SSW";
+    case WindDirection.south_west:
+      return "SW";
+    case WindDirection.west_south_west:
+      return "WSW";
+    case WindDirection.west:
+      return "W";
+    case WindDirection.north_west:
+      return "NW";
+    case WindDirection.north_north_west:
+      return "NNW";
+    default:
+      return "";
+  }
+}
+
+IconData windDir2icon(WindDirection? wd) => wd == null
+    ? WeatherIcons.na
+    : WeatherIcons.fromString('towards_${windDir2string(wd).toLowerCase()}');
