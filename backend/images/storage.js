dfb697e56dc8dc285cddbfc96a8a9d75062c4b8f
HannesH <33062605+HannesGitH@users.noreply.github.com>
2022-05-24 18:39:12 +0200

    optional

--- backend/images/storage.js
+++ backend/images/storage.js
@@ -1,6 +1,6 @@
 const pathm = require("path");
 
-const files = require("./filesystem")
+const files = require("./filesystem");
 const rootfolder = require("../db/queries").getLink;
 
 const { memorize_link } = require("./hash");
@@ -8,22 +8,25 @@ const { setMainImgByHash } = require("../api");
 
 const fs = require("fs");
 const multer = require("multer");
+const { set_first_image_as_main } = require("../options");
 
 const mstorage = multer.diskStorage({
-
   //done?: we currently store everything in the root dir, but we want to add into specific subdir that needs to be extracted from req.body.thingy.E1 etc
   destination: (req, file, cb) => {
     rootfolder(req.body).then((rf) => {
-      console.log("multi-upload",rf);
+      console.log("multi-upload", rf);
       const path = files.formatpath(pathm.join(rf.rootfolder, rf.link));
       fs.mkdirSync(path, { recursive: true });
       cb(null, path);
       //: if destination is empty -> set the new image as main (aka as req.body.Link; update)
-      fs.readdir(path,{}, (err, files) => {if (files.length < 3) {
-        rf.mainImg=file.originalname;
-        req.body.hash = memorize_link(rf);
-        setMainImgByHash(req,{},(err,res)=>{});
-      }})
+      if (set_first_image_as_main)
+        fs.readdir(path, {}, (err, files) => {
+          if (files.length < 3) {
+            rf.mainImg = file.originalname;
+            req.body.hash = memorize_link(rf);
+            setMainImgByHash(req, {}, (err, res) => {});
+          }
+        });
     });
   },
   filename: (req, file, cb) => {
@@ -32,5 +35,5 @@ const mstorage = multer.diskStorage({
 });
 
 module.exports = {
-    mstorage,
+  mstorage,
 };
