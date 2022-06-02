af5a981e92128071c6f01f6b2edbcae7ba630431
HannesGitH <33062605+HannesGitH@users.noreply.github.com>
2022-06-02 13:15:39 +0000

    Deploying to gh-pages from @ Mastbau-FN/inspector@1239795d0c2211364d1e839c34b260f3ca7cd01a 🚀

--- app/assets/lib/assets/consts.dart
+++ app/assets/lib/assets/consts.dart
@@ -2,10 +2,10 @@ import 'package:flutter/material.dart';
 
 class Options {
   static const canUseMobileNetworkIfPossible = true;
-  static var preferRemoteImages = true;
+  static var preferRemoteImages = false;
   static const debugAllResponses = false;
-  static const debugLocalMirror = false;
-  static const debugImages = true;
+  static const debugLocalMirror = true;
+  static const debugImages = false;
   static const infinitelyreloadPictures = true;
   static var reloadTries = 5;
   static var showDoggo = false;
