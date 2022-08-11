bd26623c027d4f087726c484b4cfbd5f717058fe
HannesH <33062605+HannesGitH@users.noreply.github.com>
2022-08-11 17:51:42 +0200

    ran codegen

--- frontend/lib/options.g.dart
+++ frontend/lib/options.g.dart
@@ -8,36 +8,33 @@ part of 'options.dart';
 
 Options _$OptionsFromJson(Map<String, dynamic> json) => Options()
   ..canBeOffline = json['canBeOffline'] as bool
-  ..canUseMobileNetworkIfPossible =
-      json['canUseMobileNetworkIfPossible'] as bool
-  ..preferRemote = json['preferRemoteImages'] as bool
-  ..debugAllResponses = json['debugAllResponses'] as bool
-  ..debugLocalMirror = json['debugLocalMirror'] as bool
-  ..debugImages = json['debugImages'] as bool
+  ..forceOffline = json['forceOffline'] as bool
+  ..useMobileNetworkForUpload = json['useMobileNetworkForUpload'] as bool
+  ..useMobileNetworkForDownload = json['useMobileNetworkForDownload'] as bool
+  ..preferRemote = json['preferRemote'] as bool
+  ..mergeOnline = json['mergeOnline'] as bool
+  ..mergeOnlineEvenInCached = json['mergeOnlineEvenInCached'] as bool
   ..infinitelyreloadPictures = json['infinitelyreloadPictures'] as bool
   ..reloadTries = json['reloadTries'] as int
-  ..showDoggo = json['showDoggo'] as bool
   ..no_image_placeholder_name = json['no_image_placeholder_name'] as String
-  ..mergeOnline = json['mergeLoadedDataIntoOnlineData'] as bool
-  ..mergeOnlineEvenInCached =
-      json['mergeLoadedDataIntoOnlineDataEvenInCachedParent'] as bool
-  ..useMobileNetworkForUpload = json['useMobileNetworkForUpload'] as bool
-  ..useMobileNetworkForDownload = json['useMobileNetworkForDownload'] as bool;
+  ..showDoggo = json['showDoggo'] as bool
+  ..debugAllResponses = json['debugAllResponses'] as bool
+  ..debugLocalMirror = json['debugLocalMirror'] as bool
+  ..debugImages = json['debugImages'] as bool;
 
 Map<String, dynamic> _$OptionsToJson(Options instance) => <String, dynamic>{
       'canBeOffline': instance.canBeOffline,
-      'canUseMobileNetworkIfPossible': instance.canUseMobileNetworkIfPossible,
-      'preferRemoteImages': instance.preferRemote,
+      'forceOffline': instance.forceOffline,
+      'useMobileNetworkForUpload': instance.useMobileNetworkForUpload,
+      'useMobileNetworkForDownload': instance.useMobileNetworkForDownload,
+      'preferRemote': instance.preferRemote,
+      'mergeOnline': instance.mergeOnline,
+      'mergeOnlineEvenInCached': instance.mergeOnlineEvenInCached,
+      'infinitelyreloadPictures': instance.infinitelyreloadPictures,
+      'reloadTries': instance.reloadTries,
+      'no_image_placeholder_name': instance.no_image_placeholder_name,
+      'showDoggo': instance.showDoggo,
       'debugAllResponses': instance.debugAllResponses,
       'debugLocalMirror': instance.debugLocalMirror,
       'debugImages': instance.debugImages,
-      'infinitelyreloadPictures': instance.infinitelyreloadPictures,
-      'reloadTries': instance.reloadTries,
-      'showDoggo': instance.showDoggo,
-      'no_image_placeholder_name': instance.no_image_placeholder_name,
-      'mergeLoadedDataIntoOnlineData': instance.mergeOnline,
-      'mergeLoadedDataIntoOnlineDataEvenInCachedParent':
-          instance.mergeOnlineEvenInCached,
-      'useMobileNetworkForUpload': instance.useMobileNetworkForUpload,
-      'useMobileNetworkForDownload': instance.useMobileNetworkForDownload,
     };
