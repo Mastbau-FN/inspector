3d9914762de80752b674053fb1164f1aa64810fa
MastbauVM <MastbauVM>
2022-11-11 09:42:58 +0000

    disabled mapquest reverse location query (key expired anyway)

--- backend/options.js
+++ backend/options.js
@@ -1,5 +1,5 @@
 module.exports = {
-  useReverseLocationAPI : true,
+  useReverseLocationAPI : false,
   debugImageHashes: false,
   no_image_placeholder_name: "no_default_picture_yet",
   set_first_image_as_main: true,
