7eba6c8f1a5083da7b788d99201f012b84bdafb4
MastbauVM <MastbauVM>
2023-02-10 16:44:07 +0000

    bigger effort for saving thumbs

--- backend/images/hash.js
+++ backend/images/hash.js
@@ -22,57 +22,63 @@ var cache = new NanoCache({
 
 const getFileFromHash = async (hash, compressed) => {
   console.log("gettin file from ", await getPathFromHash(hash))
-  let compressed_path = homedir+"/compressed_images/"+hash
-  if(compressed && fs.existsSync(compressed_path+'/img.heic')){
-    let img = await fsp.readFile(compressed_path+'/img.heic')
+  let compressed_path = homedir + "/compressed_images/" + hash
+  if (compressed && fs.existsSync(compressed_path + '/img.heic')) {
+    let img = await fsp.readFile(compressed_path + '/img.heic')
     return img
   }
-  
+
   let img = await imgfiler.getImageFrom(
     cache.get(hash + "r"),
     cache.get(hash + "l"),
     cache.get(hash + "f")
   );
-  if(compressed){
+  if (compressed) {
+    const orig_img = img;
     img = await sharp(img)
-    .heif({quality:1,effort:1})
-    .toBuffer();
+      .heif({ quality: 1, effort: 0 })
+      .toBuffer();
 
-    await fsp.mkdir(compressed_path,{recursive:true});
-    fsp.writeFile(compressed_path+'/img.heic',img)
+    sharp(orig_img)
+      .heif({ quality: 1, effort: 9 })
+      .toBuffer().then(async (buf) => {
+        await fsp.mkdir(compressed_path, { recursive: true });
+        fsp.writeFile(compressed_path + '/img.heic', img)
+      })
+
   }
   return img
 };
 
-function throwExpression(errorMessage){
+function throwExpression(errorMessage) {
   throw new Error(errorMessage);
 }
 
 const getPathFromHash = (hash) => {
   // console.log(hash);
   // console.log(cache.stats());
-  const  throwE = ()=>{
+  const throwE = () => {
     throw new Error("-cache miss- , we couldnt resolve where that image is supposed to live");
   }
   return {
     rootpath: cache.get(hash + "r") ?? throwE(),
-    link:     cache.get(hash + "l") ?? throwE(),
+    link: cache.get(hash + "l") ?? throwE(),
     filename: cache.get(hash + "f") ?? throwE()
   }
 };
 
 const memorize = (rootpath, link, filename) => {
   // ja ein festes salt zu nehmen ist jetzt nicht so das Wahre, vorallem wenn es hier frei einlesbar ist, aber so wichtig ist dann auch nicht
-  let key = (filename == options.no_image_placeholder_name) ? options.no_image_placeholder_name : crypto.createHash('sha1').update(rootpath + link + filename+ 'v1').digest('base64');
- 
+  let key = (filename == options.no_image_placeholder_name) ? options.no_image_placeholder_name : crypto.createHash('sha1').update(rootpath + link + filename + 'v1').digest('base64');
+
   cache.set(key + "r", rootpath);
   cache.set(key + "l", link);
   cache.set(key + "f", filename);
- 
+
   return key;
 };
 
-const memorize_link = (link)=>memorize(link.rootfolder, link.link, link.filename);
+const memorize_link = (link) => memorize(link.rootfolder, link.link, link.filename);
 
 module.exports = {
   getFileFromHash,
