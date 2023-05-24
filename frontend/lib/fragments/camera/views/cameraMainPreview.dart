daffa5c6c22d59cec5d902b6623de72e187b7974
HannesH <33062605+HannesGitH@users.noreply.github.com>
2023-05-24 18:59:10 +0200

    fixed [iOS] bzw andere apple-OSs : bilder aufnehmen/hochladen klappt nicht #116 and improved camera model

--- frontend/lib/fragments/camera/views/cameraMainPreview.dart
+++ frontend/lib/fragments/camera/views/cameraMainPreview.dart
@@ -4,6 +4,8 @@ import 'package:camera/camera.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
+import '../../../widgets/error.dart';
+
 class CameraPreviewOnly extends StatelessWidget {
   final List<Widget> children;
   const CameraPreviewOnly({this.children = const [], Key? key})
@@ -11,28 +13,33 @@ class CameraPreviewOnly extends StatelessWidget {
 
   Widget _previewWithChildren(CameraController cc) => children.isEmpty
       ? CameraPreview(cc)
-      : Material(
-          //why the hell would i need a Material, it should be an ancestor already..
-          child: Stack(
+      : Stack(
           alignment: Alignment.topRight,
           children: [
             CameraPreview(cc),
             Wrap(children: children),
           ],
-        ));
+        );
 
   @override
-  Widget build(BuildContext context) {
-    return Consumer<CameraModel>(builder: (context, model, child) {
-      return FutureBuilder(
-        future: model.start(),
-        builder: (context, AsyncSnapshot<CameraController> snapshot) =>
-            // LoadingView(),
-            snapshot.connectionState == ConnectionState.done &&
-                    snapshot.data != null
-                ? _previewWithChildren(snapshot.data!)
-                : LoadingView(),
+  Widget build(BuildContext context) => Material(
+        //why the hell would i need a Material, it should be an ancestor already..
+        child: Consumer<CameraModel>(
+          builder: (context, model, child) {
+            return FutureBuilder(
+              future: model.start(),
+              builder: (context, AsyncSnapshot<CameraController> snapshot) =>
+                  // LoadingView(),
+                  switch ((snapshot.connectionState, snapshot.data)) {
+                (ConnectionState.done, CameraController cc) =>
+                  _previewWithChildren(cc),
+                (ConnectionState.done, null) => ErrorText("no camera"),
+                (ConnectionState.waiting, _) => LoadingView(),
+                (ConnectionState.active, _) => LoadingView(),
+                (ConnectionState.none, _) => ErrorText("no camera"),
+              },
+            );
+          },
+        ),
       );
-    });
-  }
 }
