bf7e86603c00d9ad2a89358b5aae600528cd3a91
Hannes <33062605+HannesGitH@users.noreply.github.com>
2024-04-24 11:22:43 +0200

    android as dependency

--- frontend/flake.nix
+++ frontend/flake.nix
@@ -26,22 +26,51 @@
           git
         ];
 
+        android = pkgs.androidenv.composeAndroidPackages {
+          toolsVersion = "26.1.1";
+          platformToolsVersion = "34.0.5";
+          buildToolsVersions = [ "30.0.0"  "30.0.3" "31.0.0" ];
+          includeEmulator = true;
+          emulatorVersion = "34.1.9";
+          platformVersions = [ "31" "33" "34" ];
+          includeSources = false;
+          includeSystemImages = true;
+          systemImageTypes = [ "google_apis_playstore" ];
+          abiVersions = [ "x86_64" "armeabi-v7a" "arm64-v8a" ];
+          cmakeVersions = [ "3.10.2" "3.18.1" ];
+          includeNDK = true;
+          ndkVersions = [ "23.1.7779620" "22.0.7026061" ];
+          useGoogleAPIs = false;
+          useGoogleTVAddOns = false;
+          #avdmanager
+          extraLicenses = [
+              "android-sdk-preview-license"
+              "android-googletv-license"
+              "android-sdk-arm-dbt-license"
+              "google-gdk-license"
+              "intel-android-extra-license"
+              "intel-android-sysimage-license"
+              "mips-android-sysimage-license"
+            ];
+          # extras = ["extras;google;gcm"];
+        };
+
         packages = {
           apk = pkgs.stdenv.mkDerivation {
             name = "apk";
-            buildInputs = with pkgs; deps ++ [ jdk17 ];
+            buildInputs = with pkgs; deps ++ [ jdk17 android.androidsdk ];
             buildPhase = ''
               flutter build apk
             '';
             # TODO: install st that nix run .#apk opens avd
           };
-          aab = pkgs.stdenv.mkDerivation {
-            name = "aab";
-            buildInputs = with pkgs; deps ++ [ jdk17 ];
-            buildPhase = ''
-              flutter build aab
-            '';
-          };
+          # aab = pkgs.stdenv.mkDerivation {
+          #   name = "aab";
+          #   buildInputs = with pkgs; deps ++ [ jdk17 android ];
+          #   buildPhase = ''
+          #     flutter build aab
+          #   '';
+          # };
           linux = pkgs.stdenv.mkDerivation {
             name = "linux binary execqutable";
             buildInputs = with pkgs; deps ++ [  ];
