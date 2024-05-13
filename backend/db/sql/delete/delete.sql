9768b839a396e363a28dc347460e64547600d1ee
MastbauVM <MastbauVM>
2024-05-13 12:56:57 +0000

    Das wäre der fix für das problem gewesen (ohne datenverlust)

--- backend/db/sql/delete/delete.sql
+++ backend/db/sql/delete/delete.sql
@@ -4,7 +4,7 @@ AND "E1" = $2 /*neuer Parameter*/
 AND "E2" = $3 /*neuer Parameter*/
 AND "E3" = $4 /*neuer Parameter*/
 -- AND "Autor" = $5 
-AND ("Autor" = $5 OR EREArt IN (5201,5202,5203,5204))
+AND ("Autor" = $5 OR "EREArt" IN (5201,5202,5203,5204))
 --*/
 
 RETURNING *
