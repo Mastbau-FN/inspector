7730fa633fb2364a74716c2c8bb4095b7ecbe719
chrisoesterreichprog <85019773+chrisoesterreichprog@users.noreply.github.com>
2025-03-30 20:13:43 +0000

    Deploying to gh-pages from @ Mastbau-FN/inspector@645f8263ebb2b82b92573942adc0bb3a31fc2d79 🚀

--- app/flutter_bootstrap.js
+++ app/flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"e672b006cb34c921db85b8e2f482ed3144a457
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "542196515"
+    serviceWorkerVersion: "2447351217"
   }
 });
